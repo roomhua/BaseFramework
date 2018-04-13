feb6cca908358583e17f0b53302cfdebe321f543
admin <1723201948@qq.com>
2018-04-13 15:19:02 +0800

    基础框架搭建

---
+++ app/src/main/java/huahong/baseframework/view/ViewPagerForScroll.java
@@ -0,0 +1,56 @@
+package huahong.baseframework.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class ViewPagerForScroll extends ViewPager {
+    private float mDownX, mDownY;
+    private boolean noScroll = true;
+
+    public ViewPagerForScroll(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        // TODO Auto-generated constructor stub
+    }
+
+    public void setNoScroll(boolean noScroll) {
+        this.noScroll = noScroll;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        return !noScroll && super.onTouchEvent(arg0);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        return !noScroll && super.onInterceptTouchEvent(arg0);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        // TODO Auto-generated method stub
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mDownX = ev.getX();
+                mDownY = ev.getY();
+                getParent().requestDisallowInterceptTouchEvent(true);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (Math.abs(ev.getX() - mDownX) > Math.abs(ev.getY() - mDownY)) {
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                } else {
+                    getParent().requestDisallowInterceptTouchEvent(false);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                getParent().requestDisallowInterceptTouchEvent(false);
+                break;
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+}
